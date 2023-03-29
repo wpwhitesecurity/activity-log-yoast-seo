f45ab24858bfe17bb8f0007314f200b2a68548d3
Stoil Dobrev <s.n.dobreff@gmail.com>
2023-03-29 13:58:02 +0300

    Version update

--- activity-log-wp-seo.php
+++ activity-log-wp-seo.php
@@ -6,7 +6,7 @@
  * Text Domain: activity-log-wp-seo
  * Author: WP White Security
  * Author URI: https://www.wpwhitesecurity.com/
- * Version: 1.3.0
+ * Version: 1.3.1
  * License: GPL2
  * Network: true
  *
